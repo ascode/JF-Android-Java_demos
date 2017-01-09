ce3ed0802c896454c0591a2181269fa45388db88
arthurkiing <ascode@msn.cn>
2017-01-10 03:48:41 +0800

    init

---
+++ app/src/main/java/cn/bgenius/jf_android_java_demos/MainActivity.java
@@ -0,0 +1,13 @@
+package cn.bgenius.jf_android_java_demos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
