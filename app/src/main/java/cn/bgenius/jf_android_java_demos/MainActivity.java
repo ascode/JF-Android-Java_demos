574cf1638c64152b843185e949c65ed7f79b661e
arthurkiing <ascode@msn.cn>
2017-02-08 17:53:16 +0800

    s

--- app/src/main/java/cn/bgenius/jf_android_java_demos/MainActivity.java
+++ app/src/main/java/cn/bgenius/jf_android_java_demos/MainActivity.java
@@ -2,6 +2,9 @@ package cn.bgenius.jf_android_java_demos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button btn1 = (Button) findViewById(R.id.button);
+        Button btn2 = (Button) findViewById(R.id.button2);
 
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("click","btn1 clicked");
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("click","btn2 clicked");
+            }
+        });
     }
 }
